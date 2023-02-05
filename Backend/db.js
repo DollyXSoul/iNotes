3fb55dc55e5ce81a0d5547d9fb892267041e9ed0
Dolly Chauhan <abcddolly1234@gmail.com>
2023-02-05 23:26:39 +0530

    ....

--- Backend/db.js
+++ Backend/db.js
@@ -5,8 +5,8 @@ mongoose.set('strictQuery', false);
 const mongoURI = process.env.URI;
 
 const connectToMongo = () => {
-    mongoose.connect(mongoURI, () => {
-        console.log("Connected to the mongodb");
-    })
+    mongoose.connect(mongoURI)
+        .then(() => console.log('Connected to MongoDB'))
+        .catch(e => console.log(e));
 }
 module.exports = connectToMongo;
